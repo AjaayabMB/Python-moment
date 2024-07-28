47635baa8b54415b539bef7e8726cfe7a75cc829
AjaayabMB <157717154+AjaayabMB@users.noreply.github.com>
2024-07-28 19:06:13 +0545

    Day 1 Homework

--- Homework 6.py
+++ Homework 6.py
@@ -3,12 +3,12 @@
 x = int(input("Enter the number of entries:"))
 total_entries = range(0, x)
 list = []
-list_positive=[]
+list_positive = []
 for a in total_entries:
     entry = int(input("Enter a number"))
     list.append(entry)
 print(list)
 for l in list:
-    if(l>0):
-     list_positive.append(l)
+    if (l > 0):
+        list_positive.append(l)
 print(list_positive)
