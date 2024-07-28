05f41dd1b6b904e0fafd1ca695e728c27d9ff88a
AjaayabMB <157717154+AjaayabMB@users.noreply.github.com>
2024-07-28 19:02:56 +0545

    Update Homework 6.py
    Error fixed by Shreya Didi.

--- Homework 6.py
+++ Homework 6.py
@@ -3,13 +3,12 @@
 x = int(input("Enter the number of entries:"))
 total_entries = range(0, x)
 list = []
+list_positive=[]
 for a in total_entries:
     entry = int(input("Enter a number"))
     list.append(entry)
-print("Original list:")
 print(list)
-for a in total_entries:
-    if (list[a] < 0):
-        list.remove(list[a])
-print("New list:")
-print(list)
+for l in list:
+    if(l>0):
+     list_positive.append(l)
+print(list_positive)
